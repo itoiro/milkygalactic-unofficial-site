216cdba5aca73afedcb0202366d7367609da980b
utouto <h.uto.biz@gmail.com>
2025-12-11 16:11:53 +0000

    デプロイファイルの作成

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')?.[1]
+const isGithubPages = process.env.GITHUB_PAGES === 'true'
+const base = isGithubPages && repositoryName ? `/${repositoryName}/` : '/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base,
   plugins: [vue()],
   server: {
     port: 3000,
